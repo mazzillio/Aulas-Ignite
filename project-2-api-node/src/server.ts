93cf5e5c9739ad8d0aecbdefa3f2a55722526d26
mazzillio <mateca500@gmail.com>
2022-06-04 11:16:17 -0300

    refatorando routes

--- project-2-api-node/src/server.ts
+++ project-2-api-node/src/server.ts
@@ -1,10 +1,9 @@
 import express from "express";
-import { categoriesRoutes } from "./routes/categories.routes";
-import { specificationsRoutes } from "./routes/specifications.routes";
+import { routes } from "./routes";
+
 
 const app = express();
 
 app.use(express.json());
-app.use("/categories",categoriesRoutes);
-app.use("/specifications",specificationsRoutes)
+app.use(routes)
 app.listen(3500, () => console.log("serve is open"));
