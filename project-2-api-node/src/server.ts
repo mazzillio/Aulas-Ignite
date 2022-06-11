46e05379bf711fe16b7b68b52747d89cd61854e8
mazzillio <mateca500@gmail.com>
2022-06-11 11:12:18 -0300

    Adicionando a ferramenta de swagger para documentação da API

--- project-2-api-node/src/server.ts
+++ project-2-api-node/src/server.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { routes } from "./routes";
+import swaggerFile from "./Swagger.json";
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.listen(3500, () => console.log("serve is open"));
