9c90a9feace4c072a7009192ccccfa248fffbe8b
mazzillio <mateca500@gmail.com>
2022-06-11 10:50:33 -0300

    Pegando as informações do client e repassando para o back end e utilizando promises para esperar resolver as operações de stream

--- project-2-api-node/src/modules/cars/useCases/importCategory/index.ts
+++ project-2-api-node/src/modules/cars/useCases/importCategory/index.ts
@@ -1,7 +1,9 @@
+import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
 import { ImportCategoryController } from "./ImportCategoryController";
 import { ImportCategoryService } from "./ImportCategoryService";
 
-const importCategoryService = new ImportCategoryService();
+const categoriesRpository = CategoriesRepository.getInstance();
+const importCategoryService = new ImportCategoryService(categoriesRpository);
 const importCategoryController = new ImportCategoryController(
   importCategoryService
 );
