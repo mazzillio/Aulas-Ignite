bfc89ac046c5ba25f29ba90413bf390d26862870
mazzillio <mateca500@gmail.com>
2022-06-12 11:11:03 -0300

    Retirando a pasta de tmp

--- project-2-api-node/src/modules/cars/useCases/importCategory/ImportCategoryService.ts
+++ project-2-api-node/src/modules/cars/useCases/importCategory/ImportCategoryService.ts
@@ -26,6 +26,7 @@ export class ImportCategoryService {
           });
         })
         .on("end", () => {
+          fs.promises.unlink(file.path);
           resolve(categories);
         })
         .on("error", (err) => {
