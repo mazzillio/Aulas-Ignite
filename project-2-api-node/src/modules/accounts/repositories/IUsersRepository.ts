a040b785d99f9399f3bc8817c6cd9dcf85557fbd
mazzillio <mateca500@gmail.com>
2022-07-03 17:36:36 -0300

    Caso de uso de criação de usuário

--- project-2-api-node/src/modules/accounts/repositories/IUsersRepository.ts
+++ project-2-api-node/src/modules/accounts/repositories/IUsersRepository.ts
@@ -1,5 +1,7 @@
 import { ICreateUserDTO } from "../dtos/ICreateUserDTO";
+import { User } from "../model/User";
 
 export interface IUsersRepository {
   create(dataRequest: ICreateUserDTO): Promise<void>;
+  findByEmail(email: string): Promise<User>;
 }
--- project-2-api-node/src/modules/accounts/useCases/createUser/createUserService.ts
+++ project-2-api-node/src/modules/accounts/useCases/createUser/createUserService.ts
@@ -1,3 +1,4 @@
+import { hash } from "bcrypt";
 import { inject, injectable } from "tsyringe";
 
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
@@ -10,6 +11,17 @@ export class CreateUserService {
     private usersRepository: IUsersRepository
   ) {}
   async execute(dataCreateRequest: ICreateUserDTO): Promise<void> {
-    await this.usersRepository.create(dataCreateRequest);
+    const { name, email, password, driver_license } = dataCreateRequest;
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+    if (userAlreadyExists) {
+      throw new Error("User already exists!");
+    }
+    const hasedPassword = await hash(password, 8);
+    await this.usersRepository.create({
+      name,
+      email,
+      password: hasedPassword,
+      driver_license,
+    });
   }
 }
