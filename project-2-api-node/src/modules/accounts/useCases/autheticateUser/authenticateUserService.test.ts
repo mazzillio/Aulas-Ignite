4da74ea7df5f8ba20a0ad844cedd5a3db10198a2
mazzillio <mateca500@gmail.com>
2022-08-20 20:56:41 -0300

    Criação dos testes para authenticated user

---
+++ project-2-api-node/src/modules/accounts/useCases/autheticateUser/authenticateUserService.test.ts
@@ -0,0 +1,54 @@
+import { AppError } from "../../../../Errors/AppError";
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { UsersRepositoryInMemory } from "../../repositories/implementations/UsersRepositoryInMemory";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { CreateUserService } from "../createUser/createUserService";
+import { AuthenticateUserService } from "./authenticateUserService";
+
+describe("Authenticate user", () => {
+  let usersRepository: IUsersRepository;
+  let createUserService: CreateUserService;
+  let authenticateUserService: AuthenticateUserService;
+  beforeEach(() => {
+    usersRepository = new UsersRepositoryInMemory();
+    createUserService = new CreateUserService(usersRepository);
+    authenticateUserService = new AuthenticateUserService(usersRepository);
+  });
+  it("should be able to authenticate a user", async () => {
+    const user: ICreateUserDTO = {
+      name: "test",
+      email: "user@test.com",
+      password: "1234",
+      driver_license: "0001234",
+    };
+    await createUserService.execute(user);
+    const responseAuth = await authenticateUserService.execute({
+      email: user.email,
+      password: user.password,
+    });
+    expect(responseAuth).toHaveProperty("token");
+  });
+  it("should not be able authenticated a non existsent user", async () => {
+    expect(async () => {
+      await authenticateUserService.execute({
+        email: "algum@email.com",
+        password: "123",
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+  it("should not be able authenticated a password incorrect", () => {
+    const user: ICreateUserDTO = {
+      name: "userTest",
+      email: "algum@email.com",
+      password: "1234",
+      driver_license: "0001234",
+    };
+    expect(async () => {
+      await createUserService.execute(user);
+      await authenticateUserService.execute({
+        email: user.email,
+        password: "123",
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+});
